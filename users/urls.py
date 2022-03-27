38ef48c442a4429dd6258142198f60cd363cf609
glory665 <glory665@mail.ru>
2022-03-27 14:30:15 +0300

    lesson_1

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from users.views import login, registration, profile, logout
+import users.views
+from users.views import login, registration, profile, logout, verify
 
 app_name = 'users'
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path('registration/', registration, name='registration'),
     path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout'),
+
+    path('veryfy/<str:email>/<str:activate_key>/', verify, name='verify'),
 ]
