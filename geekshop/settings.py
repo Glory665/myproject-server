fae58b93f752418c9ae2fb9f262d96c0e82b5286
glory665 <glory665@mail.ru>
2022-06-27 00:15:23 +0300

    added cache

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
@@ -201,3 +203,17 @@ if DEBUG:
         'debug_toolbar.panels.profiling.ProfilingPanel',
         'template_profiler_panel.panels.template.TemplateProfilerPanel',
     ]
+
+if os.name == 'posix':
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_SECONDS = 10
+    CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+
+    CACHE = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
+    }
+
+LOW_CACHE = True
--- products/views.py
+++ products/views.py
@@ -2,13 +2,30 @@ from django.shortcuts import render
 
 from products.models import ProductCategory, Product
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.cache import cache
+from django.conf import settings
+from django.views.decorators.cache import cache_page, never_cache
 
 
+# def get_links_menu(category_id=None):
+#     if settings.LOW_CACHE:
+#        key = 'categories'
+#         categories = cache.get(key)
+#         if categories is None:
+#             categories = Product.objects.filter(category_id=category_id)
+#             cache.set(key, categories)
+#         return categories
+#     else:
+#         return Product.objects.filter(category_id=category_id)
+
+
+@never_cache
 def index(request):
     context = {'title': 'GeekShop'}
     return render(request, 'products/index.html', context)
 
 
+@cache_page(3600)
 def products(request, category_id=None, page=1):
     if category_id:
         products = Product.objects.filter(category_id=category_id)
