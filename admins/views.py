0e83353415b8874efbd92608b1f52f552dceb6c2
glory665 <glory665@mail.ru>
2022-06-28 22:30:19 +0300

    added product_is_active

--- admins/views.py
+++ admins/views.py
@@ -1,3 +1,6 @@
+from django.db import connection
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
@@ -6,8 +9,26 @@ from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from products.models import ProductCategory
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}: ')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
 
 
 @user_passes_test(lambda u: u.is_staff)
