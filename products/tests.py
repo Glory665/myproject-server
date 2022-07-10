2b8ea117130a69e267a68372a2b3d3e0a326e78f
glory665 <glory665@mail.ru>
2022-07-10 21:07:37 +0300

    added test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,33 @@
+from django.core.management import call_command
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+from products.models import ProductCategory, Product
+
+
+class TestProductSmoke(TestCase):
+    def setUp(self):
+        call_command('flush', '--noinput')
+        call_command('loaddata', 'test_db.json')
+        self.client = Client()
+
+    def test_product_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/products/category/0/')
+        self.assertEqual(response.status_code, 200)
+
+        for category in ProductCategory.objects.all():
+            response = self.client.get(f'/products/category/{category.pk}/')
+            self.assertEqual(response.status_code, 200)
+
+        for product in Product.objects.all():
+            response = self.client.get(f'/products/{product.pk}/')
+            self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        call_command('products', 'admins', 'ordersapp', 'baskets')
